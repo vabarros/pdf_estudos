a2acf0bc410ba58069d250e64c69e895a523c731
vabarros <victor.abarros@gmail.com>
2025-06-08 13:24:01 +0100

    adjustment about mvn

--- backend/src/main/java/com/quiz/generator/backend/config/WebConfig.java
+++ backend/src/main/java/com/quiz/generator/backend/config/WebConfig.java
@@ -10,7 +10,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // Permite CORS para todos os endpoints
-                .allowedOrigins("http://localhost:5500", "https://quiz-ctfl-at.netlify.app","http://localhost:63342") // Ajuste conforme seu frontend
+                .allowedOrigins("http://localhost:5500",
+                        "http://localhost:63342",
+                        "https://*.netlify.app",
+                        "http://localhost:8080", // Para testes locais do frontend (se você abre diretamente o HTML)
+                        "http://localhost:3000") // Ajuste conforme seu frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
