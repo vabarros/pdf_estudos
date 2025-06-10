3e566a7a4b2fca19feb423bec64466797f034eb6
vabarros <victor.abarros@gmail.com>
2025-06-10 23:46:53 +0100

    feat: update API to openAI

--- backend/src/main/java/com/quiz/generator/backend/config/WebConfig.java
+++ backend/src/main/java/com/quiz/generator/backend/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/api/**") // Permite CORS para todos os endpoints
                 .allowedOrigins("http://localhost:5500",
                         "http://localhost:63342",
-                        "https://quiz-backend-api-a23j.onrender.com", // Adicionar o próprio domínio do backend (às vezes necessário para alguns cenários)
+                        "https://quiz-backend-api-a23j.onrender.com/api", // Adicionar o próprio domínio do backend (às vezes necessário para alguns cenários)
                         "https://quiz-frontend-app.onrender.com" ) // Ajuste conforme seu frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
